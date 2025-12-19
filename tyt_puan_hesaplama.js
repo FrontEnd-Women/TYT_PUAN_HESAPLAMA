c94abc7234e219710055e2ac7cdcbd1da9f087ca
EVİN DOĞAL <44978770+FrontEnd-Women@users.noreply.github.com>
2025-12-19 23:47:39 +0300

    tyt_puan_hesaplama.js

--- tyt_puan_hesaplama.js
+++ tyt_puan_hesaplama.js
@@ -10,7 +10,7 @@ Sonuç
 */
 
 
-/*
+
 let turkceDoğruSayisi, turkceYanlisSayisi=0;
 let MatDoğruSayisi, MatYanlisSayisi=0;
 let fenDoğruSayisi, fenYanlisSayisi=0;
@@ -21,38 +21,8 @@ let yeniSatir="\r\n";
 let Mesaj ="TYT Puan Uygulamasına Hoşgeldiniz..."+
 yeniSatir+"1: Puan Hesapla"+yeniSatir+"2: Çıkış Yap";
 
-let secim =prompt(Mesaj);
-switch(secim){
-    case "1":
-        okulpuani=Number(prompt("Okul Puanını Giriniz :"));
-        turkceDoğruSayisi=Number(prompt("Türkçe Doğru Sayısını Giriniz :"));
-        turkceYanlisSayisi=Number(prompt("Türkçe Yanlış Sayısını Giriniz :"));
 
-        MatDoğruSayisi=Number(prompt("Matematik Doğru Sayısını Giriniz :"));
-        MatYanlisSayisi=Number(prompt("Matematik Yanlış Sayısını Giriniz :"));
 
-        fenDoğruSayisi=Number(prompt("Fen Doğru Sayısını Giriniz :"));
-        fenYanlisSayisi=Number(prompt("Fen Yanlış Sayısını Giriniz :"));
-
-        sosyalDoğruSayisi=Number(prompt("Sosyal Doğru Sayısını Giriniz :"));
-        sosyalYanlisSayisi=Number(prompt("Sosyal Yanlış Sayısını Giriniz :"));
-
-        let dogruSayisi=turkceDoğruSayisi+MatDoğruSayisi+fenDoğruSayisi+sosyalDoğruSayisi;
-        let yanlisSayisi=turkceYanlisSayisi+MatYanlisSayisi+fenYanlisSayisi+sosyalYanlisSayisi;
-        let KalanDogruSayisi=dogruSayisi-(yanlisSayisi/4);
-        let puan=(KalanDogruSayisi*4)+okulpuani+SabitPuan;
-        alert("TYT Puanınız : "+puan)
-        break;
-    case "2":
-        alert("Başarılı Bir Şekilde Çıkış Yaptınız...")
-        break;
-    default:
-        alert("Geçerli Bir Değer Giriniz!")
-        break;        
-}
-*/
-
-/*
 function puanHesapla() {
     let SabitPuan = 100;
 
@@ -77,28 +47,6 @@ function puanHesapla() {
 
     document.getElementById("sonuc").innerText = "TYT Puanınız: " + puan.toFixed(2);
 }
-    */
+    
 
 
-//Harf sayısı  bulma
-
-/* bana ne lazım
-1 tane fonksiyon
-1 tane metin
-kullanıcıdan harf almak
-*/
-
-let text="Bugün JavaScriptte birsürü proje yaptım.Çok eğlenceliydi.";
-let harf=prompt("Bir harf giriniz :");
-let sonuc=found(harf);
-alert("Harf sayısı : "+ sonuc);
-function found(harf) {
-    let toplam=0;
-    for(let i=0; i<text.length; i++){
-        if(text.charAt(i).toLocaleLowerCase()==harf.toLocaleLowerCase()){
-            toplam+=1;
-
-        }
-    }
-    return toplam;
-}
